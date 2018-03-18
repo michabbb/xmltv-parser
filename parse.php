845233fa034b1c9db06dff723f5977e7a89fc94c
micha <info@macropage.de>
2018-03-18 14:02:29 +0100

    add offset

--- parse.php
+++ parse.php
@@ -3,6 +3,7 @@ require 'vendor/autoload.php';
 
 $Parser = new \macropage\xmltv\parser\parser();
 $Parser->setFile($argv[1]);
+$Parser->setOffset('P0Y0DT1H0M0S');
 $Parser->setChannelfilter('prosiebenmaxx.de'); //optional
 try {
 	$Parser->parse();
