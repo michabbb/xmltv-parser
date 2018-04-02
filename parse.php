0fea588ac688ff469b9ed54732bd6b75f535cb38
micha <info@macropage.de>
2018-04-02 18:36:56 +0200

    fix timezone conversion

--- parse.php
+++ parse.php
@@ -3,8 +3,8 @@ require 'vendor/autoload.php';
 
 $Parser = new \macropage\xmltv\parser\parser();
 $Parser->setFile($argv[1]);
-$Parser->setOffset('P0Y0DT1H0M0S'); //optional default = 1 hour
-$Parser->setChannelfilter('prosiebenmaxx.de'); //optional
+$Parser->setTargetTimeZone('Europe/Berlin');
+//$Parser->setChannelfilter('prosiebenmaxx.de'); //optional
 $Parser->setIgnoreDescr('Keine Details verfügbar.'); //optional
 try {
 	$Parser->parse();
